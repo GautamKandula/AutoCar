04cf0cc6a1941b035bbaab7170d4748444916345
Gautam Kandula <164435366+GautamKandula@users.noreply.github.com>
2025-08-08 18:14:35 -0400

    Update 2 - Road Creation
    Created road background with lines using linear interpolation

--- main.js
+++ main.js
@@ -4,14 +4,18 @@ canvas.height = window.innerHeight;
 canvas.width = 200; // TBD on size
 
 const ctx = canvas.getContext("2d");
-const car = new Car(100, 100, 30, 50);
+const road = new Road(canvas.width/2, canvas.width*0.9);
+const car = new Car(road.getLaneCenter(2), 100, 30, 50);
 car.draw(ctx);
 
 animate(); // called on loop to simulate real time copy
 
 function animate() {
     car.update();
+
     canvas.height = window.innerHeight; // Clears Canvas after animation frame (no leftover images)
+    
+    road.draw(ctx)
     car.draw(ctx);
     requestAnimationFrame(animate); // Sets animate to animation frame
 }
